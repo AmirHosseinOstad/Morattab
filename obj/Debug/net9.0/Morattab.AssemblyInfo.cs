540f8288124a3b0d99950213f5a171c8d82f80ac
AmirHosseinOstad <amirostad2021@hotmail.com>
2025-08-29 01:33:55 +0330

    fix bug

--- obj/Debug/net9.0/Morattab.AssemblyInfo.cs
+++ obj/Debug/net9.0/Morattab.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Morattab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c11ed7d39c39ef18b6a70bf887923055a060a8f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Morattab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Morattab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
