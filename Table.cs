4443d72cde227b189a2304edd5848352a156601a
AmirHosseinOstad <amirostad2021@hotmail.com>
2025-08-29 17:47:10 +0330

    Adding the table class and the ability to add values ​​to the object and display the object

---
+++ Table.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morattab
+{
+    /// <summary>
+    /// Defining a table, adding rows, and displaying them
+    /// </summary>
+    public class Table
+    {
+        private List<string[]> TableList = new List<string[]>();
+
+        /// <summary>
+        /// Adding rows to a table, each row is a string array and each array index is a column of the table
+        /// </summary>
+        /// <param name="Record">Array of columns in a row of a table</param>
+        public void Add(string[] Record)
+        {
+            TableList.Add(Record);
+        }
+
+        /// <summary>
+        /// Sort and print the table according to the added values
+        /// </summary>
+        /// <param name="RowLength">The row length to which the spacing is adjusted (defaults to 0 for automatic mode)</param>
+        /// <param name="RowsSeparator">Show or hide table row separator line</param>
+        /// <param name="SeparatorLetter">Column separator character</param>
+        /// <param name="RowSeparatorLetter">Row separator character</param>
+        public void Write(int RowLength = 0, bool? RowsSeparator = false, char? SeparatorLetter = '|', char? RowSeparatorLetter = '-')
+        {
+            //Find max length
+            int[] MaxLength = new int[TableList.Last().Length];
+
+            foreach (var item in TableList)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (MaxLength[i] == 0)
+                    {
+                        MaxLength[i] = item[i].Length;
+                    }
+                    else
+                    {
+                        if (item[i].Length > MaxLength[i])
+                        {
+                            MaxLength[i] = item[i].Length;
+                        }
+                    }
+                }
+            }
+
+
+            foreach (var item in TableList)
+            {
+                string Row = "";
+                for (int i = 0; i < item.Length; i++)
+                {
+                    string Spaces = "";
+                    for (int j = 0; j < MaxLength[i] - item[i].Length; j++)
+                    {
+                        Spaces += " ";
+                    }
+
+                    Row += item[i] + Spaces + $" {SeparatorLetter} ";
+                }
+
+                Console.WriteLine(Row);
+
+                if ((bool)RowsSeparator)
+                {
+                    for(int i = 0;i < Row.Length; i++)
+                    {
+                        Console.Write(RowSeparatorLetter);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
--- obj/Debug/net9.0/Morattab.AssemblyInfo.cs
+++ obj/Debug/net9.0/Morattab.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Morattab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86c9de75d44fb98a2a8630deeccc05525b80773")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11ef18ce2a583c8d382f5c886f1fa8f4e5102ad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Morattab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Morattab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
